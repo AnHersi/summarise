cafbcc60095b1c07704dacde251cfb1ac344db0c
Anas Hersi <eng_anas12@hotmail.com>
2023-04-21 09:17:16 +0100

    Remove unused imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SummariesModule } from './summaries/summaries.module';
 import { ConfigModule } from '@nestjs/config';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -13,7 +11,5 @@ import { AppService } from './app.service';
     MongooseModule.forRoot(process.env.MONGO_URI),
     SummariesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
